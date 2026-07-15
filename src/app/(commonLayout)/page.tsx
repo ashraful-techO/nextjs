3a427c9579ef1bdb4a8b647da7f746f3781bbb63
ashraful-techO <islam.ashraful.cse.97@gmail.com>
2026-07-15 09:04:42 +0600

    feat(auth): implement login and registration forms with Google authentication
    - Added LoginForm component for user login with email and Google.
    - Added RegisterForm component for user registration with email and password.
    - Updated login and register pages to use the new form components.
    - Integrated authClient for session management and Google login functionality.
    - Created reusable Card and Field components for better UI structure.
    - Established a basic auth client configuration for API interactions.

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const getSession = await authClient.getSession();
+
+  console.log({ getSession });
   return (
     <div className="container mx-auto px-4">
       <h1>Hello World</h1>
