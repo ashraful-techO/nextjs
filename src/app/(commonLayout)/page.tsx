ec1e73e4ae14b74cf08a001f3f5db199b1f3da59
ashraful-techO <islam.ashraful.cse.97@gmail.com>
2026-07-25 19:46:19 +0600

    feat: add blog functionality with BlogCard component and blog service
    - Introduced BlogCard component to display individual blog posts.
    - Implemented blogService to fetch blog posts from the API.
    - Updated Home page to render a grid of BlogCard components.
    - Created blog.type.ts for BlogPost interface definition.
    - Added Badge component for displaying tags and featured status.
    - Modified pnpm-workspace.yaml to disable builds for specific packages.

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,18 +1,24 @@
+import BlogCard from "@/components/module/homepage/BlogCard";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { blogService } from "@/services/blog.service";
 import { userService } from "@/services/user.service";
+import { BlogPost } from "@/types";
 import { cookies } from "next/headers";
 import Image from "next/image";
 
 export default async function Home() {
   // const { data, error } = await userService.getSession();
 
-  // console.log({ data });
+  const { data } = await blogService.getBlogPost();
+
+  console.log(data?.data);
 
   return (
-    <div className="container mx-auto px-4">
-      <h1>Hello World</h1>
-      <Button>Click me</Button>
+    <div className="grid grid-cols-3 mx-auto max-w-7xl gap-3">
+      {data?.data?.map((post: BlogPost) => {
+        return <BlogCard key={post.id} post={post} />;
+      })}
     </div>
   );
 }
