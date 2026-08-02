08c0b1320e979872a34ca45603b790c9e8252c61
ashraful-techO <islam.ashraful.cse.97@gmail.com>
2026-08-03 00:18:27 +0600

    added

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client";
+
+import { getBlogs } from "@/actions/blog.action";
+import { blogService } from "@/services/blog.service";
+import React, { useEffect, useState } from "react";
 
 export default function AboutPage() {
-  return (
-    <div className="container mx-auto px-4">
-      aboutPage
-    </div>
-  );
+  const [data, setData] = useState();
+  const [error, setError] = useState<{ message: string } | null>(null);
+
+  console.log(data);
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
+      setData(data);
+      setError(error);
+    })();
+  }, []);
+  return <div className="container mx-auto px-4">aboutPage</div>;
 }
